0176dbc699bbb0af8ba80be88071d4118fa3e15b
NLDPhat <1312422@student.hcmus.edu.vn>
2016-10-02 02:22:50 +0700

    Code with bootstrap

--- MyBlog/config/routes.rb
+++ MyBlog/config/routes.rb
@@ -3,7 +3,10 @@ Rails.application.routes.draw do
     resources :comments
   end
   resources :users
-  get 'welcome/index'
+  get 'blog' => "blogs#index"
+  get 'about' => "pages#about", as: :about
+  get 'contact' => "pages#contact", as: :contact
   root 'welcome#index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
