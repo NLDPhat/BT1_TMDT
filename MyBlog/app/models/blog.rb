f449df2525182d18144e737b68b67711abc3a7c4
Brising <lonhthu74@gmail.com>
2016-10-02 21:53:27 +0700

    Success Repair

--- MyBlog/app/models/blog.rb
+++ MyBlog/app/models/blog.rb
@@ -2,6 +2,6 @@ class Blog < ApplicationRecord
     belongs_to :user
     has_many :comments
     validates :Title, presence: true,
-                length: { maximum: 100 }
+                length: { maximum: 200}
     validates :post, presence: true
 end
