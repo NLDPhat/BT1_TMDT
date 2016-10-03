1ba054bb3b7f9630658274f150d480688f509987
NLDPhat <1312422@student.hcmus.edu.vn>
2016-10-03 08:40:47 +0700

    Update_Delete_Blog

--- MyBlog/app/models/blog.rb
+++ MyBlog/app/models/blog.rb
@@ -1,6 +1,6 @@
 class Blog < ApplicationRecord
     belongs_to :user
-    has_many :comments
+    has_many :comments, :dependent => :destroy
     validates :Title, presence: true,
                 length: { maximum: 200}
     validates :post, presence: true
