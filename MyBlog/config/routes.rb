e69c803d320662f46dcf81fbd679fd1a01bf18a3
NLDPhat <1312422@student.hcmus.edu.vn>
2016-10-03 14:43:44 +0700

    update

--- MyBlog/config/routes.rb
+++ MyBlog/config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   get 'blog' => "blogs#index"
   get 'about' => "pages#about", as: :about
   get 'contact' => "pages#contact", as: :contact
+  get 'register' => "users#new", as: :register
   root 'welcome#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
